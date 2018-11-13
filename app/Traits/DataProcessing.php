94ade82aead0a0041d536856545af4dd06fe328f
Deák Zsolt <zsoltd@4choice.com>
2018-11-13 22:35:53 +0100

    Update DataProcessing to use real values and add by Bid

--- app/Traits/DataProcessing.php
+++ app/Traits/DataProcessing.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\DB;
 trait DataProcessing
 {
 
-//    TODO refactor this to normal response
     /**
      * @param $datas
      *
@@ -21,6 +20,7 @@ trait DataProcessing
      */
     private function organizePairData($datas, $limit=999)
     {
+        // TODO refactor this to normal response
         $ret = array();
         foreach ($datas as $data) {
             $ret[$data->exchange_id]['timestamp'][]   = $data->buckettime;
@@ -84,21 +84,56 @@ trait DataProcessing
         $offset = ($current_time - ($timeSlice * $limit)) -1;
 
         $results = DB::select(DB::raw("
-              SELECT 
+              SELECT
                 exchange_id,
-                SUBSTRING_INDEX(GROUP_CONCAT(CAST(bid AS CHAR) ORDER BY created_at), ',', 1 ) AS `open`,
+                SUBSTRING_INDEX(GROUP_CONCAT(CAST(open AS CHAR) ORDER BY datetime), ',', 1 ) AS `open`,
+                SUBSTRING_INDEX(GROUP_CONCAT(CAST(high AS CHAR) ORDER BY high DESC), ',', 1 ) AS `high`,
+                SUBSTRING_INDEX(GROUP_CONCAT(CAST(low AS CHAR) ORDER BY low), ',', 1 ) AS `low`,
+                SUBSTRING_INDEX(GROUP_CONCAT(CAST(close AS CHAR) ORDER BY datetime DESC), ',', 1 ) AS `close`,
+                SUM(basevolume) AS volume,
+                ROUND((CEILING(UNIX_TIMESTAMP(`datetime`) / $timeSlice) * $timeSlice)) AS buckettime
+              FROM tickers
+              WHERE symbol = '$pair'
+              AND UNIX_TIMESTAMP(`datetime`) > ($offset)
+              GROUP BY exchange_id, buckettime
+              ORDER BY buckettime DESC
+          "));
+
+        return $this->organizePairData($results);
+    }
+
+    /**
+     * Returns latest data
+     *
+     * @param string $pair
+     * @param int $limit
+     * @param string $periodSize
+     * @return array
+     */
+    public function getLatestDataByBid($pair='BTC/USD', $limit=168, $periodSize='1m') {
+
+        $time = $this->periodSize($periodSize);
+        $timeSlice = $time['timeslice'];
+
+        $current_time = time();
+        $offset = ($current_time - ($timeSlice * $limit)) -1;
+
+        $results = DB::select(DB::raw("
+              SELECT
+                exchange_id,
+                SUBSTRING_INDEX(GROUP_CONCAT(CAST(bid AS CHAR) ORDER BY datetime), ',', 1 ) AS `open`,
                 SUBSTRING_INDEX(GROUP_CONCAT(CAST(bid AS CHAR) ORDER BY bid DESC), ',', 1 ) AS `high`,
                 SUBSTRING_INDEX(GROUP_CONCAT(CAST(bid AS CHAR) ORDER BY bid), ',', 1 ) AS `low`,
-                SUBSTRING_INDEX(GROUP_CONCAT(CAST(bid AS CHAR) ORDER BY created_at DESC), ',', 1 ) AS `close`,
+                SUBSTRING_INDEX(GROUP_CONCAT(CAST(bid AS CHAR) ORDER BY datetime DESC), ',', 1 ) AS `close`,
                 SUM(basevolume) AS volume,
-                ROUND((CEILING(UNIX_TIMESTAMP(`created_at`) / $timeSlice) * $timeSlice)) AS buckettime,
+                ROUND((CEILING(UNIX_TIMESTAMP(`datetime`) / $timeSlice) * $timeSlice)) AS buckettime,
                 round(AVG(bid),11) AS avgbid,
                 round(AVG(ask),11) AS avgask,
                 AVG(baseVolume) AS avgvolume
               FROM tickers
               WHERE symbol = '$pair'
-              AND UNIX_TIMESTAMP(`created_at`) > ($offset)
-              GROUP BY exchange_id, buckettime 
+              AND UNIX_TIMESTAMP(`datetime`) > ($offset)
+              GROUP BY exchange_id, buckettime
               ORDER BY buckettime DESC
           "));
 
