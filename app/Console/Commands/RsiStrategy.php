41a4e547102d2ecdda8a5d2e00e7b45b49170e0f
Deák Zsolt <zsoltd@4choice.com>
2020-08-10 22:01:18 +0200

    RSI strategy implementation as test gose below 70 Sell goes up on 30 Buy

---
+++ app/Console/Commands/RsiStrategy.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Exchanges;
+use App\Traits\DataProcessing;
+use App\Utils\Strategies;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class RsiStrategy extends Command
+{
+    use DataProcessing;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'trade:rsi';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Simple strategy to trade on RSI, now just under test.';
+
+    /**
+     * @var Strategies
+     */
+    private $strategies;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param Strategies $strategies
+     */
+    public function __construct(Strategies $strategies)
+    {
+        $this->strategies = $strategies;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $exchangeId = Exchanges::where('slug', 'poloniex')->first()->id;
+
+        while (1) {
+
+
+            $results = $this->getLatestData('BTC/USDT', 50, '5m');
+
+            $response = $this->strategies->strategy_rsi($results[$exchangeId]);
+            if (is_array($response)) {
+                $isUp = $response['isUp'] ? '<fg=blue>isUp:true</>' : '<fg=red>isUp:false</>';
+                $isDown = $response['isDown'] ? '<fg=blue>isDown:true</>' : '<fg=red>isDown:false</>';
+                $this->line(
+                    '<fg=green>[' . Carbon::now(
+                    ) . '] [' . $response['previousRsi'] . '] [' . $response['latestRsi'] . ']</> - ' . $isUp . ' - ' . $isDown . ' -- ' . implode(
+                        ",",
+                        $response['rsi']
+                    ) . "\n"
+                );
+
+                if ($response['isUp']) {
+                    $this->error('Sell!');
+                } // if
+
+                if ($response['isDown']) {
+                    $this->line('<bg=green>Buy!</>');
+                } // if
+
+
+            } else {
+                $this->error($response);
+            } // if
+
+            sleep(5);
+        } // while
+    }
+}
