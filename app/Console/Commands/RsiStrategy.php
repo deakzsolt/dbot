a7876b2435ea3c7f66a50a121f03396f481505ba
Deák Zsolt <zsoltd@4choice.com>
2020-08-13 18:45:19 +0200

    Update RSI strategy, add yarn, update gitignore

--- app/Console/Commands/RsiStrategy.php
+++ app/Console/Commands/RsiStrategy.php
@@ -53,33 +53,46 @@ class RsiStrategy extends Command
 
         while (1) {
 
+            $timeFrames = [
+                '5m',
+                '15m',
+                '30m',
+                '1h',
+            ];
 
-            $results = $this->getLatestData('BTC/USDT', 50, '5m');
+            foreach($timeFrames as $timeFrame) {
+                $results = $this->getLatestData('BTC/USDT', 50, $timeFrame);
 
-            $response = $this->strategies->strategy_rsi($results[$exchangeId]);
-            if (is_array($response)) {
-                $isUp = $response['isUp'] ? '<fg=blue>isUp:true</>' : '<fg=red>isUp:false</>';
-                $isDown = $response['isDown'] ? '<fg=blue>isDown:true</>' : '<fg=red>isDown:false</>';
-                $this->line(
-                    '<fg=green>[' . Carbon::now(
-                    ) . '] [' . $response['previousRsi'] . '] [' . $response['latestRsi'] . ']</> - ' . $isUp . ' - ' . $isDown . ' -- ' . implode(
-                        ",",
-                        $response['rsi']
-                    ) . "\n"
-                );
+                if ($results) {
+                    $response = $this->strategies->strategy_rsi($results[$exchangeId]);
+                    if (is_array($response)) {
+                        $isUp = $response['isUp'] ? '<fg=blue>isUp:true</>' : '<fg=red>isUp:false</>';
+                        $isDown = $response['isDown'] ? '<fg=blue>isDown:true</>' : '<fg=red>isDown:false</>';
+                        $this->line(
+                            '<fg=green>[' . Carbon::now(
+                            ) . '] [' . $response['previousRsi'] . '] [' . $response['latestRsi'] . ']</> - ' . $isUp . ' - ' . $isDown . ' -- ' . implode(
+                                ",",
+                                $response['rsi']
+                            ) . "\n"
+                        );
 
-                if ($response['isUp']) {
-                    $this->error('Sell!');
-                } // if
+                        if ($response['isUp']) {
+                            $this->error('Sell!');
+                        } // if
 
-                if ($response['isDown']) {
-                    $this->line('<bg=green>Buy!</>');
-                } // if
+                        if ($response['isDown']) {
+                            $this->line('<bg=green>Buy!</>');
+                        } // if
 
-
-            } else {
-                $this->error($response);
-            } // if
+                    } else {
+                        $this->error($response);
+                        exit();
+                    } // if
+                } else {
+                    $this->error('There are not enough data to show up calculations.');
+                    exit();
+                }
+            } // foreach
 
             sleep(5);
         } // while
